ea6cd78e501e2b729089455376ed23b91467c764
unknown <ahmedmiraz87@gmail.com>
2026-04-22 16:56:01 +0600

    arithmatic_mathoperator

---
+++ arithmatic_mathoperator.py
@@ -0,0 +1,30 @@
+import numpy as np
+x= np.array([10,20,30,70,16])
+y= np.array([2,3,20,50,20])
+
+add =x+y
+sub=x-y
+multi=x*y
+div=x/y
+modular=x%y
+sin_val= np.sin(x)
+base_10_log_values= np.log10(x)
+sqr_root= np.sqrt(x)
+
+conv_deg= np.rad2deg(x) #converted radiun to deg
+
+print(add)
+print(sub)
+print(multi)
+print(div)
+print(modular)
+print(sin_val)
+print(conv_deg)
+print(base_10_log_values)
+print(sqr_root)
+
+
+#mathmatical function
+
+
+
